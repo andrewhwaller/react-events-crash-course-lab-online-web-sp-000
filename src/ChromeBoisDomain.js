7a7c82cb690c381fb641c92f4a31ce54a9a39c42
Andrew Waller <andrewhwaller@gmail.com>
2019-09-26 03:33:27 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -27,7 +27,7 @@ export default class ChromeBoisDomain extends Component {
       <canvas
         onMouseMove={this.handleMouseMove}
         onClick={() => {toggleCycling()}}
-        onKeyDown{( => {this.handleKeyDown()})}
+        onKeyDown{() => {this.handleKeyDown()})}
         width='900'
         height='600'
         tabIndex="0">
